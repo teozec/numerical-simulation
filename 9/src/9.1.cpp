3a42cbb75f6d41aeb2fe2473c481c829f95fabe1
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-08-19 17:31:25 +0200

    Start exercise 9

---
+++ 9/src/9.1.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include "TSP.h"
+
+using namespace std;
+
+double cost_accumulate(vector<Individual> individuals, int n_individuals)
+{
+	return accumulate(individuals.begin(), individuals.begin() + n_individuals, individuals[0].cost(),
+		[](const double x, const Individual &y) { return x + y.cost();}) / n_individuals;
+}
+
+void TSP(Random rnd, const int n_cities, const int n_individuals, const int n_generations,
+	vector<City> city, string s)
+{
+	vector<Individual> individuals;
+	for (int i = 0; i < n_individuals; i++)
+		individuals.push_back(Individual{city, rnd});
+
+	Population population{individuals, rnd};
+	ofstream out{"data/9.1-" + s + "-out.dat"};
+	if (!out.is_open()) {
+		cerr << "Could not open data/9.1-" + s + "-out.dat" << endl;
+		exit(1);
+	}
+
+	out << population.individuals[0].cost() << '\t' << cost_accumulate(population.individuals, n_individuals / 2) << endl;
+	for (int i = 0; i < n_generations; i++) {
+		population.new_generation();
+		out << population.individuals[0].cost() << '\t' << cost_accumulate(population.individuals, n_individuals / 2) << endl;
+	}
+
+	ofstream best{"data/9.1-" + s + "-best.dat"};
+	if (!best.is_open()) {
+		cerr << "Could not open data/9.1-" + s + "-best.dat" << endl;
+		exit(1);
+	}
+	for (int i = 0; i < n_cities; i++)
+		best << population.individuals[0].cities[i].label << endl;
+}
+
+int main()
+{
+	Random rnd(SEED_DIR "/Primes", SEED_DIR "/seed.in");
+	const int n_cities = 32;
+	const int n_individuals = 5000;
+	const int n_generations = 300;
+
+	vector<City> circ;
+	ofstream out_cities{"data/9.1-circ-cities.dat"};
+	if (!out_cities.is_open()) {
+		cerr << "Could not open data/9.1-circ-cities.dat" << endl;
+		exit(1);
+	}
+	for (int i = 0; i < n_cities; i++) {
+		double theta = rnd.rannyu(0, 2*M_PI);
+		circ.push_back(City{cos(theta), sin(theta), i});
+		out_cities << i << '\t' << cos(theta) << '\t' << sin(theta) << endl;
+	}
+	TSP(rnd, n_cities, n_individuals, n_generations, circ, "circ");
+
+	vector<City> square;
+	out_cities.close();
+	out_cities.open("data/9.1-square-cities.dat");
+	if (!out_cities.is_open()) {
+		cerr << "Could not open data/9.1-square-cities.dat" << endl;
+		exit(1);
+	}
+	for (int i = 0; i < n_cities; i++) {
+		double x = rnd.rannyu();
+		double y = rnd.rannyu();
+		square.push_back(City{x, y, i});
+		out_cities << i << '\t' << x << '\t' << y << endl;
+	}
+	TSP(rnd, n_cities, n_individuals, n_generations, square, "square");
+
+	return 0;
+}
---
+++ 9/src/TSP.h
@@ -0,0 +1,127 @@
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include "random.h"
+
+struct City {
+	double x, y;
+	int label;
+
+	City(double x, double y, int label): x{x}, y{y}, label{label} {}
+
+	double distance(const City &other) const {
+		return std::abs(x - other.x) + std::abs(y - other.y);
+	}
+
+	bool operator==(const City &other) {
+		return label == other.label;
+	}
+};
+
+class Individual {
+public:
+	double cost() const {
+		double c = cities[cities.size()].distance(cities[0]);
+		for (size_t i = 0; i < cities.size() - 1; i++)
+			c += cities[i].distance(cities[i+1]);
+		return c;
+	}
+
+	std::vector<City> cities;
+
+	Individual() {}
+	Individual(std::vector<City> c, Random &rnd) {
+		cities.push_back(c[0]);
+		for (size_t i = 1; i < c.size(); i++) {
+			for (;;) {
+				City city = c[(size_t) rnd.rannyu(1, c.size())];
+				if (std::find(cities.begin() + 1, cities.end(), city) == cities.end()) {
+					cities.push_back(city);
+					break;
+				}
+			}
+		}
+	}
+
+	bool operator<(const Individual &other) const {
+		return cost() < other.cost();
+	}
+
+	bool operator>(const Individual &other) const {
+		return cost() > other.cost();
+	}
+
+	bool operator==(const Individual &other) const {
+		const double epsilon = 1e-8;
+		return std::abs(cost() - other.cost() < epsilon);
+	}
+
+	bool operator!=(const Individual &other) const {
+		return !(*this == other);
+	}
+};
+
+class Population {
+public:
+	std::vector<Individual> individuals;
+
+private:
+	size_t n_individuals, n_cities;
+	Random &rnd;
+	double pm, pc, p;
+
+public:
+	Population(std::vector<Individual> individuals, Random &rnd, double pm = 0.1, double pc = 0.5, double p = 3.):
+		individuals{individuals}, n_individuals{individuals.size()}, n_cities{individuals[0].cities.size()}, rnd{rnd}, pm{pm}, pc{pc}, p{p} {
+		std::sort(individuals.begin(), individuals.end());
+	}
+
+	Individual select() {
+		return individuals[(size_t) n_individuals * std::pow(rnd.rannyu(), p)];
+	}
+
+	void new_generation() {
+		std::vector<Individual> new_generation_vec{n_individuals};
+		for (size_t i = 0; i < n_individuals; i += 2) {
+			Individual parent1 = select();
+			Individual parent2 = select();
+			Individual son1, son2;
+			crossover(parent1, parent2, son1, son2);
+			mutate(son1);
+			mutate(son2);
+			new_generation_vec[i] = son1;
+			new_generation_vec[i+1] = son2;
+		}
+		std::sort(new_generation_vec.begin(), new_generation_vec.end());
+		individuals = new_generation_vec;
+	}
+
+	void mutate(Individual &individual) {
+		if (rnd.rannyu() < pm)
+			std::swap(individual.cities[(size_t) rnd.rannyu(1, n_cities - 1)], individual.cities[(size_t) rnd.rannyu(1, n_cities - 1)]);
+	}
+
+	void crossover(Individual parent1, Individual parent2, Individual &son1, Individual &son2) {
+		son1 = parent1;
+		son2 = parent2;
+
+		if (rnd.rannyu() < pc) {
+			size_t p_cut = (size_t) rnd.rannyu(1, n_cities - 1);
+
+			size_t index = p_cut;
+
+			for (size_t i = 0; i < n_cities; i++) {
+				City c = parent2.cities[i];
+		  		if (std::find(parent1.cities.begin(), parent1.cities.begin() + p_cut, c) == parent1.cities.begin() + p_cut)
+					son1.cities[index++] = c;
+			}
+
+			index = p_cut;
+			for (size_t i = 0; i < n_cities; i++) {
+				City c = parent1.cities[i];
+				if (std::find(parent2.cities.begin(), parent2.cities.begin() + p_cut, c) == parent2.cities.begin() + p_cut)
+					son2.cities[index++] = c;
+			}
+		}
+	}
+};
