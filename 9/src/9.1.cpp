dba987837a067e57ba11e443e54e0714976bb43e
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-09-01 16:28:28 +0200

    Comment exercise 9

--- 9/src/9.1.cpp
+++ 9/src/9.1.cpp
@@ -5,21 +5,28 @@
 #include <numeric>
 #include "TSP.h"
 
+// Exercise 9.1
+// Traveling salesman problem
+
 using namespace std;
 
 double cost_accumulate(vector<Individual> individuals, int n_individuals)
 {
+	// Use the C++ accumulate function to sum the costs of the individuals in the vector, then divide by their number
 	return accumulate(individuals.begin(), individuals.begin() + n_individuals, individuals[0].cost(),
 		[](const double x, const Individual &y) { return x + y.cost(); }) / n_individuals;
 }
 
+// TSP algorithm
 void TSP(Random rnd, const int n_cities, const int n_individuals, const int n_generations,
 	vector<City> city, string s, double pm, double pc, double p)
 {
+	// Populate a vector of individuals (each individual is a random path to connect all cities)
 	vector<Individual> individuals;
 	for (int i = 0; i < n_individuals; i++)
 		individuals.push_back(Individual{city, rnd});
 
+	// Create a population object, containing the vector of individuals
 	Population population{individuals, rnd, pm, pc, p};
 	ofstream out{"data/9.1-" + s + "-out.dat"};
 	if (!out.is_open()) {
@@ -27,7 +34,10 @@ void TSP(Random rnd, const int n_cities, const int n_individuals, const int n_ge
 		exit(1);
 	}
 
+	// Write to the output file the cost of the best individual and the mean cost of the best half of the population
 	out << population.individuals[0].cost() << '\t' << cost_accumulate(population.individuals, n_individuals / 2) << endl;
+
+	// Evolve the population for n_generations
 	for (int i = 0; i < n_generations; i++) {
 		population.new_generation();
 		out << population.individuals[0].cost() << '\t' << cost_accumulate(population.individuals, n_individuals / 2) << endl;
@@ -38,6 +48,7 @@ void TSP(Random rnd, const int n_cities, const int n_individuals, const int n_ge
 		cerr << "Could not open data/9.1-" + s + "-best.dat" << endl;
 		exit(1);
 	}
+	// Write the best path to the output file
 	for (int i = 0; i < n_cities; i++)
 		best << population.individuals[0].cities[i].label << endl;
 }
@@ -55,11 +66,14 @@ int main()
 		cerr << "Could not open data/9.1-circ-cities.dat" << endl;
 		exit(1);
 	}
+
+	// Generate cities placed on a circumference
 	for (int i = 0; i < n_cities; i++) {
 		double theta = rnd.rannyu(0, 2*M_PI);
 		circ.push_back(City{cos(theta), sin(theta), i});
 		out_cities << i << '\t' << cos(theta) << '\t' << sin(theta) << endl;
 	}
+	// Call TSP algorithm
 	TSP(rnd, n_cities, n_individuals, n_generations, circ, "circ", 0.15, 0.7, 2.);
 
 	vector<City> square;
@@ -69,12 +83,14 @@ int main()
 		cerr << "Could not open data/9.1-square-cities.dat" << endl;
 		exit(1);
 	}
+	// Generate cities placed inside a square
 	for (int i = 0; i < n_cities; i++) {
 		double x = rnd.rannyu();
 		double y = rnd.rannyu();
 		square.push_back(City{x, y, i});
 		out_cities << i << '\t' << x << '\t' << y << endl;
 	}
+	// Call TSP algorithm
 	TSP(rnd, n_cities, n_individuals, n_generations, square, "square", 0.1, 0.7, 2.);
 
 	return 0;
--- 9/src/TSP.h
+++ 9/src/TSP.h
@@ -3,12 +3,14 @@
 #include <algorithm>
 #include "random.h"
 
+// A city consists of x, y coordinates and an integer label
 struct City {
 	double x, y;
 	int label;
 
 	City(double x, double y, int label): x{x}, y{y}, label{label} {}
 
+	// The L1 distance between two cities
 	double distance(const City &other) const {
 		return std::abs(x - other.x) + std::abs(y - other.y);
 	}
@@ -18,8 +20,10 @@ struct City {
 	}
 };
 
+// An individual is an ordered path of cities
 class Individual {
 public:
+	// The cost of an individual is the sum of the distance between cities i and i+1
 	double cost() const {
 		double c = cities[cities.size()].distance(cities[0]);
 		for (size_t i = 0; i < cities.size() - 1; i++)
@@ -30,11 +34,15 @@ public:
 	std::vector<City> cities;
 
 	Individual() {}
+
+	// Constructor for Individual
+	// It takes a vector of cities as inupt, and stores them in a random order (using rnd), only keeping the first one in the first position.
 	Individual(std::vector<City> c, Random &rnd) {
 		cities.push_back(c[0]);
 		for (size_t i = 1; i < c.size(); i++) {
 			for (;;) {
-				City city = c[(size_t) rnd.rannyu(1, c.size())];
+				City city = c[(size_t) rnd.rannyu(1, c.size())];	// Extract random city...
+				// .. and push it back if it has not been extracteb yet
 				if (std::find(cities.begin() + 1, cities.end(), city) == cities.end()) {
 					cities.push_back(city);
 					break;
@@ -43,6 +51,8 @@ public:
 		}
 	}
 
+	// operator<, operator>, operator== and operator!= compare the costs of two individuals.
+	// In particular, operator< can be used by the C++ std::sort function.
 	bool operator<(const Individual &other) const {
 		return cost() < other.cost();
 	}
@@ -61,6 +71,7 @@ public:
 	}
 };
 
+// A population contains a vector of individuals that evolve following a genetic algortithm
 class Population {
 public:
 	std::vector<Individual> individuals;
@@ -68,37 +79,49 @@ public:
 private:
 	size_t n_individuals, n_cities;
 	Random &rnd;
-	double pm, pc, p;
+	double pm;	// Mutation probability
+	double pc;	// Crossover probability
+	double p;	// Selection parameter
 
 public:
+	// Construt a population from a vector of individuals
 	Population(std::vector<Individual> individuals, Random &rnd, double pm = 0.1, double pc = 0.5, double p = 3.):
 		individuals{individuals}, n_individuals{individuals.size()}, n_cities{individuals[0].cities.size()}, rnd{rnd}, pm{pm}, pc{pc}, p{p} {
-		std::sort(individuals.begin(), individuals.end());
+		std::sort(individuals.begin(), individuals.end());	// The individuals in a population are always sorted from the least costly. std::sort uses Individual::operator<.
 	}
 
+	// Select an individual randomly, with a greater probability of extracting a less costly one
 	Individual select() {
 		return individuals[(size_t) n_individuals * std::pow(rnd.rannyu(), p)];
 	}
 
+	// Evolve the population
 	void new_generation() {
 		std::vector<Individual> new_generation_vec{n_individuals};
 		for (size_t i = 0; i < n_individuals; i += 2) {
+			// Select two parents
 			Individual parent1 = select();
 			Individual parent2 = select();
 			Individual son1, son2;
+			// Create son1 and son2 from the parents, possibly with crossover
 			crossover(parent1, parent2, son1, son2);
+			// Mutate the sons
 			mutate(son1);
 			mutate(son2);
+			// Add the sons to the new generation
 			new_generation_vec[i] = son1;
 			new_generation_vec[i+1] = son2;
 		}
-		std::sort(new_generation_vec.begin(), new_generation_vec.end());
-		individuals = new_generation_vec;
+		std::sort(new_generation_vec.begin(), new_generation_vec.end());	 // Sort the population
+		individuals = new_generation_vec;					 // Replace the old generation
 	}
 
+	// Mutate an individual (each mutation happens with probability pm)
 	void mutate(Individual &individual) {
+		// Swap two random cities
 		if (rnd.rannyu() < pm)
 			std::swap(individual.cities[(size_t) rnd.rannyu(1, n_cities)], individual.cities[(size_t) rnd.rannyu(1, n_cities)]);
+		// Invert a group of contiguous cities
 		if (rnd.rannyu() < pm) {
 			int r1 = (int) rnd.rannyu(1, n_cities);
 			int r2 = (int) rnd.rannyu(1, n_cities);
@@ -107,6 +130,7 @@ public:
 			else
 				std::reverse(individual.cities.begin()+r2, individual.cities.begin()+r1);
 		}
+		// Rotate a group of contiguous cities
 		if (rnd.rannyu() < pm) {
 			std::vector<int> x{(int) rnd.rannyu(1, n_cities), (int) rnd.rannyu(1, n_cities), (int) rnd.rannyu(1, n_cities)};
 			std::sort(x.begin(), x.end());
@@ -114,15 +138,18 @@ public:
 		}
 	}
 
+	// Create two sons from the parents, applying crossover with probability pc
 	void crossover(Individual parent1, Individual parent2, Individual &son1, Individual &son2) {
 		son1 = parent1;
 		son2 = parent2;
 
+		// Crossover
 		if (rnd.rannyu() < pc) {
-			size_t p_cut = (size_t) rnd.rannyu(1, n_cities - 1);
+			size_t p_cut = (size_t) rnd.rannyu(1, n_cities - 1);	// Random cutting point
 
 			size_t index = p_cut;
 
+			// Place cities after p_cut in son1 depending on the position they appear in parent2
 			for (size_t i = 0; i < n_cities; i++) {
 				City c = parent2.cities[i];
 		  		if (std::find(parent1.cities.begin(), parent1.cities.begin() + p_cut, c) == parent1.cities.begin() + p_cut)
@@ -130,6 +157,7 @@ public:
 			}
 
 			index = p_cut;
+			// Place cities after p_cut in son2 depending on the position they appear in parent1
 			for (size_t i = 0; i < n_cities; i++) {
 				City c = parent1.cities[i];
 				if (std::find(parent2.cities.begin(), parent2.cities.begin() + p_cut, c) == parent2.cities.begin() + p_cut)
